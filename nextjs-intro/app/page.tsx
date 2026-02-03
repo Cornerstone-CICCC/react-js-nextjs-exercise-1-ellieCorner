2c9ecd778cb449afd8fc5d03411773739a3a8559
ellieCorner <ellie25corner@gmail.com>
2026-02-03 00:05:08 -0800

    React JS - NextJS Exercise 1

---
+++ nextjs-intro/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold mb-4">Welcome to Our Home Page</h1>
+      <p className="text-lg text-gray-600">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+    </div>
+  );
+}
